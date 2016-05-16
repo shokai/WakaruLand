5ee15182356a5b62d2d78cca36e5d45127619a2d
Naoaki Yamada <napo0703@gmail.com>
2016-05-16 13:51:18 +0900

    express init

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'jade');
+
+app.get('/', function (req, res) {
+  res.render('index', {title: 'Hey', message: 'Hello there!'});
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
